300fe283b711b6eb91ab331aaf01540c9be92238
wapmesquita <wapmesquita@github.com>
2014-02-07 00:11:21 -0200

    .

--- src/br/com/dxt/domain/MyAbstractEntity.java
+++ src/br/com/dxt/domain/MyAbstractEntity.java
@@ -4,12 +4,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.SequenceGenerator;
 
 @MappedSuperclass
 public abstract class MyAbstractEntity {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GERAL_SEQ")
+	@SequenceGenerator(name = "GERAL_SEQ", sequenceName = "geral_seq", allocationSize = 1)
 	public Long id;
 
 }
